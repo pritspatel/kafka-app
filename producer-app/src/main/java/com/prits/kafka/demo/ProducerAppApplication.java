c9d35e345c912f96fd8467d4b596e98c5c02f1d2
Pritesh Patel <3ppatel@gmail.com>
2017-06-18 09:19:06 -0400

    Initial commit

---
+++ producer-app/src/main/java/com/prits/kafka/demo/ProducerAppApplication.java
@@ -0,0 +1,12 @@
+package com.prits.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerAppApplication.class, args);
+	}
+}
